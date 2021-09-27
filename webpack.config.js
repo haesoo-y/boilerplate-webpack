f0ee60bba50eb29f6a0c0ad8be7d3274d46dd45c
Harry <haesoo9410@gmail.com>
2021-09-27 20:48:36 +0900

    refactor: Update webpack config for dev & prod mode 환경변수를 사용한 개발모드와 배포모드 분리

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,55 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  devtool: "source-map",
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        options: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                targets: {
-                  browsers: ["> 10% in KR"],
+module.exports = () => {
+  const prod = process.env.NODE_ENV === "production";
+  const mode = prod ? "production" : "development";
+  const devtool = prod ? "hidden-source-map" : "eval";
+  const cssLoader = [];
+  const plugins = [];
+  if (prod) {
+    cssLoader.push(MiniCssExtractPlugin.loader, "css-loader", "sass-loader");
+    plugins.push(new HtmlWebpackPlugin({ template: "./index.html" }), new MiniCssExtractPlugin());
+  } else {
+    cssLoader.push("style-loader", "css-loader", "sass-loader");
+    plugins.push(new HtmlWebpackPlugin({ template: "./index.html" }));
+  }
+  return {
+    mode,
+    devtool,
+    entry: "./src/index.js",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    browsers: ["> 10% in KR"],
+                  },
+                  debug: true,
                 },
-                debug: true,
-              },
+              ],
             ],
-          ],
-          plugins: ["@babel/plugin-proposal-class-properties"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+          exclude: /node_modules/,
         },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "app.js",
-  },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: cssLoader,
+        },
+      ],
+    },
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "app.js",
+    },
+    plugins,
+  };
 };
